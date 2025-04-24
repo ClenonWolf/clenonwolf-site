ebc7b03967b694d42144f7e71ab9f71c93fca14c
ClenonWolf <mail@clenonwolf.com>
2025-04-24 11:24:22 +0200

    added possibly the worst way to change the post view order

--- art_posts.php
+++ art_posts.php
@@ -16,12 +16,28 @@
     <div class="centerdiv">
         <p>Testing page for viewing posts I've uploaded through my posting system. Posts are read from the sqlite db and each loaded.</p>
         <p>Different ways to sort and proper post views with post information coming soon :3</p>
+        <form action="art_posts.php" method="post">
+            <label for="sortby">Sort by:</label>
+            <select onchange="this.form.submit();" name="sortby">
+                <option value='SELECT * FROM art_posts ORDER BY id DESC'>ID Descending</option>
+                <option value='SELECT * FROM art_posts ORDER BY id ASC'>ID Ascending</option>
+                <option value='SELECT * FROM art_posts ORDER BY creation_date ASC'>Creation Date Ascending</option>
+                <option value='SELECT * FROM art_posts ORDER BY creation_date DESC'>Creation Date Descending</option>
+                <option value='SELECT * FROM art_posts ORDER BY upload_date ASC' >Upload Date Ascending</option>
+                <option value='SELECT * FROM art_posts ORDER BY upload_date DESC'>Upload Date Descending</option>
+            </select>
+        </form>
         <div class=imageContainer>
             <?php
             include "sql_init.php";
             include "regen_thumbs.php";
+            if(isset($_POST["sortby"])) {
+                $sql = $_POST["sortby"];
+            } else {
+                $sql = 'SELECT * FROM art_posts ORDER BY id DESC';
+            }
             $db = new SQLite3('sqlite/db.sqlite');
-            $results = $db->query('SELECT * FROM "art_posts" ORDER BY "id" DESC');
+            $results = $db->query($sql);
             while ($row = $results->fetchArray()) {
                 echo "<a style='padding:7px' target='_blank' href='media/uploads/{$row['file_hash']}/{$row['file_name']}'><img src='media/uploads/thumbs/{$row['file_hash']}' width=10% ></a>";
             }
